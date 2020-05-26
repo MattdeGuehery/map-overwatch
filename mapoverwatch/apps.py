b216f4acb84bda9ccd78d55fa39cefc1e6dc3fe5
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-05-26 17:06:50 -0400

    renaming folder

--- mapoverwatch/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MapoverwatchConfig(AppConfig):
-    name = 'mapoverwatch'
---
+++ mapoverwatchapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MapoverwatchappConfig(AppConfig):
+    name = 'mapoverwatchapp'
