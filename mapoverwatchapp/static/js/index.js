6203cbd37d5402c4bcbe2259431d7dc345e655e6
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-01 21:59:22 -0400

    setup app bar structure

--- mapoverwatchapp/static/js/index.js
+++ mapoverwatchapp/static/js/index.js
@@ -1,10 +1,11 @@
 const app = new Vue({
-	delimiters: ['${', '}'],
+	delimiters: ['[[', ']]'],
 	vuetify: new Vuetify({}),
 	el: '#app',
 	data: () => ({
 		source: true,
 		drawer: null,
+		title: 'Overwatch Maps'
 	}),
 	computed: {
 
@@ -14,7 +15,7 @@ const app = new Vue({
 	},
 	template: `
 	<v-app id="app">
-		<v-navigation-drawer v-model="drawer" app>
+		<!-- <v-navigation-drawer v-model="drawer" app>
 			<v-list dense>
 				<v-list-item link>
 					<v-list-item-action>
@@ -33,24 +34,32 @@ const app = new Vue({
 					</v-list-item-content>
 				</v-list-item>
 			</v-list>
-		</v-navigation-drawer>
+		</v-navigation-drawer> -->
 
 		<v-app-bar app color="indigo" dark>
-			<v-app-bar-nav-icon @click.stop="drawer=!drawer"></v-app-bar-nav-icon>
-		<v-toolbar-title>Application</v-toolbar-title>
+			<!-- <v-app-bar-nav-icon @click.stop="drawer=!drawer"></v-app-bar-nav-icon> -->
+			<v-toolbar-title>[[title]]</v-toolbar-title>
+			<template v-slot:extension>
+				<v-tabs align-with-title>
+				<v-tab>Home</v-tab>
+				<v-tab>Map Callouts</v-tab>
+				<v-tab>Top Down Maps</v-tab>
+				</v-tabs>
+			</template>
 		</v-app-bar>
 
 		<v-content>
 			<v-container class="fill-height" fluid>
 				<v-row align="center" justify="center">
 					<v-col class="text-center">
+						
 					</v-col>
 				</v-row>
 			</v-container>
 		</v-content>
-		<v-footer color="indigo" app>
+		<!-- <v-footer color="indigo" app>
 			<span class="white--text">&copy; 2019</span>
-		</v-footer>
+		</v-footer> -->
 	</v-app>
 	`
 });
