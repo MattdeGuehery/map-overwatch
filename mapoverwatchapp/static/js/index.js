272dd8f0f8c67e6ccb3ed0b32a21beed3f1e9aab
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-01 21:49:34 -0400

    removing unused features on home page

--- mapoverwatchapp/static/js/index.js
+++ mapoverwatchapp/static/js/index.js
@@ -44,14 +44,6 @@ const app = new Vue({
 			<v-container class="fill-height" fluid>
 				<v-row align="center" justify="center">
 					<v-col class="text-center">
-						<v-tooltip left>
-							<template v-slot:${on}">
-								<v-btn :href="source" icon large target="_blank" v-on="on">
-									<v-icon large>mdi-code-tags</v-icon>
-								</v-btn>
-							</template>
-							<span>Source</span>
-						</v-tooltip>
 					</v-col>
 				</v-row>
 			</v-container>
