a5d0521d0b9ee2f43da988a6c38d5715d46a182f
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-01 22:35:23 -0400

    setting up home page and adding basic landing page

---
+++ mapoverwatchapp/static/js/home.js
@@ -0,0 +1,21 @@
+var homecards = Vue.component('home-cards', {
+	data: () => ({
+
+	}),
+	computed: {
+		tabCards: function () {
+			let tabCards = [];
+			tabCards.push({ name: 'Map Callouts', text: '', id: 1 });
+			tabCards.push({ name: 'Top Down Maps', text: '', id: 2 });
+			return tabCards;
+		}
+	},
+	template: `
+	<div>
+		<v-card class="mx auto my-2" v-for="(card, index) in tabCards" :key="index">
+			<v-card-title>{{card.name}}</v-card-title>
+			<v-card-text>{{card.text}}</v-card-text>
+		</v-card>
+	</div>
+	`
+})
