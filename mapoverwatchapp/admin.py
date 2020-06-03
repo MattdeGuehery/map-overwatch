4003bc8c9d606de1f9498a2bf757cafc425dc487
Chris Bardeloza <cbardeloza2@gmail.com>
2020-06-02 23:36:29 -0500

    Model changes for admin.py

--- mapoverwatchapp/admin.py
+++ mapoverwatchapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(map_table)
+admin.site.register(image_table)
+
 # Register your models here.
--- mapoverwatchapp/models.py
+++ mapoverwatchapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class map_table(models.Model):
+    map_name= models.CharField(max_length=120)
+    map_id= models.IntegerField()
+
+    Assult='AS'
+    Escort='ES'
+    Control='CN'
+    Escort_hybrid='EH'
+    Arena='AR'
+
+    MAP_TYPE=[
+        (Assult, 'Assult'),
+        (Escort, 'Escort'),
+        (Control, 'Control'),
+        (Escort_hybrid, 'Escort Hybrid'),
+        (Arena, 'Arena'),
+    ]
+
+    map_type= models.CharField(
+        max_length=2,
+        choices= MAP_TYPE,
+    )
+
+class image_table(models.Model):
+    image_id= models.IntegerField()
+    image_map= models.CharField(max_length=120)
+    image_url= models.URLField(max_length=300)
+    image_order= models.IntegerField()
+    isTopDown= models.BooleanField(default=False)
