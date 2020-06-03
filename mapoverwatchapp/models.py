c8e1c194ae90bca76c2c4692711a6108f7ab3fc3
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-03 00:41:29 -0400

    formatting

--- mapoverwatchapp/models.py
+++ mapoverwatchapp/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
 class map_table(models.Model):
-    map_name= models.CharField(max_length=120)
-    map_id= models.IntegerField()
+    map_name = models.CharField(max_length=120)
+    map_id = models.IntegerField()
 
-    Assult='AS'
-    Escort='ES'
-    Control='CN'
-    Escort_hybrid='EH'
-    Arena='AR'
+    Assult = 'AS'
+    Escort = 'ES'
+    Control = 'CN'
+    Escort_hybrid = 'EH'
+    Arena = 'AR'
 
-    MAP_TYPE=[
+    MAP_TYPE = [
         (Assult, 'Assult'),
         (Escort, 'Escort'),
         (Control, 'Control'),
@@ -19,14 +21,15 @@ class map_table(models.Model):
         (Arena, 'Arena'),
     ]
 
-    map_type= models.CharField(
+    map_type = models.CharField(
         max_length=2,
-        choices= MAP_TYPE,
+        choices=MAP_TYPE,
     )
 
+
 class image_table(models.Model):
-    image_id= models.IntegerField()
-    image_map= models.CharField(max_length=120)
-    image_url= models.URLField(max_length=300)
-    image_order= models.IntegerField()
-    isTopDown= models.BooleanField(default=False)
+    image_id = models.IntegerField()
+    image_map = models.CharField(max_length=120)
+    image_url = models.URLField(max_length=300)
+    image_order = models.IntegerField()
+    isTopDown = models.BooleanField(default=False)
