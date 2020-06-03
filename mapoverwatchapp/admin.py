924e11d905e6e6748b344cebb8b7d049bc08a9d1
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-03 01:03:22 -0400

    moving model registation below comment

--- mapoverwatchapp/admin.py
+++ mapoverwatchapp/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
+# Register your models here.
 admin.site.register(map_table)
 admin.site.register(image_table)
-
-# Register your models here.
