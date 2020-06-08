92d78c7ac42e8371dc7c72b05d5f4a9f811446ba
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-08 01:50:26 -0400

    fixing spacing

--- mapoverwatchapp/views.py
+++ mapoverwatchapp/views.py
@@ -9,18 +9,17 @@ from mapoverwatchapp.models import map_table, image_table, MAP_TYPE
 logger = logging.getLogger(__name__)
 
 def maps(request):
-    data_from_db = map_table.objects.all()
-    formatted_data = []
-    for map in data_from_db:
-        formatted_data.append({
-            'map_name': map.map_name,
-            'map_id': map.map_id,
-            'map_type': dict(MAP_TYPE).get(map.map_type),
+	data_from_db = map_table.objects.all()
+	formatted_data = []
+	for map in data_from_db:
+		formatted_data.append({
+			'map_name': map.map_name,
+			'map_id': map.map_id,
+			'map_type': dict(MAP_TYPE).get(map.map_type),
 			'map_url': 'mapcallouts/' + str(map.map_id)
-        })
-
-    context = {'data': formatted_data}
-    return render(request, 'maps.html', context)
+		})
+	context = {'data': formatted_data}
+	return render(request, 'maps.html', context)
 
 
 def mapcallouts(request, map_id):
