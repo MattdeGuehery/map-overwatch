0303de11d37bd3ae078b6c0c865d2f554911f316
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-07 21:07:54 -0400

    updating views to use new env variable

--- mapoverwatchapp/urls.py
+++ mapoverwatchapp/urls.py
@@ -5,7 +5,5 @@ from . import models
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # adding example endpoint. This will probably change
-    path('api/maps', views.get_maps, name='get_maps'),
     path('mapcallouts/', views.mapcallouts, name='mapcallouts'),
 ]
