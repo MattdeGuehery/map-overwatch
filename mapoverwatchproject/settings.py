8f673c7b5937662e2ae678b507add6a5117dd36c
Matthew de Guehery <matthew.deguehery@rfsmart.com>
2020-06-11 00:56:06 -0400

    removing secret key

--- mapoverwatchproject/settings.py
+++ mapoverwatchproject/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-# 'u@(wqah58i#9v^69de7-g3s_xpox_8v_u_)br$=+j97(9s@cs4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', False)
